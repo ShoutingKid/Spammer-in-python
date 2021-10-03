3014d642563403729065c9e4e44694508152affa
Shouting Kid <91276621+ShoutingKid@users.noreply.github.com>
2021-10-03 15:25:14 +0530

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -5,6 +5,6 @@ time.sleep(5) #You can adjust time.sleep() as per your choice
 while True:
     pyauto.write("Hello World") #You can type in whatever message you want to send
     pyauto.press("enter")
-    time.sleep(1)
+    time.sleep(1) #You can adjust the time as per your need
     
 #And now you are ready to trouble your friends!!    
